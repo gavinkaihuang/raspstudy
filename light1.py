1e83d1742ae76755635c8b4472e3e3ec421dc7ab
huangkai <gavinkaihuang@126.com>
2020-03-20 01:40:29 +0800

    add control strong for light funciton to light1.py

--- light1.py
+++ light1.py
@@ -11,7 +11,7 @@ def first_light():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.OUT)
     GPIO.output(11, True)
-    time.sleep(8)
+    time.sleep(3)
     GPIO.output(11, False)
     GPIO.cleanup()
 
@@ -35,8 +35,27 @@ def flash_light():
     finally:
         GPIO.cleanup()
 
+def control_light_strong():
+    led_pin = 11
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(led_pin, GPIO.OUT)
+
+    pwm_led = GPIO.PWM(led_pin, 500)
+    pwm_led.start(100)
+
+    try:
+        while True:
+            duty_s = input("请输入你要的亮度（0-100）：")
+            duty = int(duty_s)
+            pwm_led.ChangeDutyCycle(duty)
+    finally:
+        GPIO.cleanup()
+
+
 if __name__ == '__main__':
     first_light()
 
     flash_light()
+
+    control_light_strong()
     pass
