e376f6c21b4b895c0cfe06848bf4ef4b764528b6
huangkai <gavinkaihuang@126.com>
2020-03-20 01:04:35 +0800

    update light1.py use function

--- light1.py
+++ light1.py
@@ -2,9 +2,15 @@ import RPi.GPIO as GPIO
 
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-GPIO.output(11, True)
-time.sleep(8)
-GPIO.output(11, False)
-GPIO.cleanup()
+if __name__ == '__main__':
+    first_light()
+    pass
+
+def first_light():
+    print("run fist light")
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT)
+    GPIO.output(11, True)
+    time.sleep(8)
+    GPIO.output(11, False)
+    GPIO.cleanup()
