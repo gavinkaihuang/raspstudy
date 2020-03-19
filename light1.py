59f4afb6e036a40734292a9ecb9d06dd250bd698
huangkai <gavinkaihuang@126.com>
2020-03-20 01:12:09 +0800

    add flash light function for light1.py

--- light1.py
+++ light1.py
@@ -13,6 +13,28 @@ def first_light():
     GPIO.output(11, False)
     GPIO.cleanup()
 
+def flash_light():
+    print("Now try to flash light")
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(11, GPIO.OUT)
+
+    i = 0
+    try:
+        while True:
+
+            if i > 10:
+                break
+            GPIO.output(11, True)
+            time.sleep(0.5)
+            GPIO.output(11, False)
+            time.sleep(0.5)
+            i += 1
+
+    finally:
+        GPIO.cleanup()
+
 if __name__ == '__main__':
     first_light()
+
+    flash_light()
     pass
