9a0a5778748eff55bc52a7d4845ba76df59dd1bf
huangkai <gavinkaihuang@126.com>
2020-03-20 17:37:25 +0800

    debug picamera

--- picamera.py
+++ picamera.py
@@ -1,8 +1,20 @@
-from picamera import PiCamera
-from time import sleep
+# from picamera import PiCamera
+# from time import sleep
+#
+# camera = PiCamera()
+#
+# camera.start_preview()
+# sleep(5)
+# camera.stop_preview()
 
-camera = PiCamera()
+#!/usr/bin/python
 
-camera.start_preview()
-sleep(5)
-camera.stop_preview()
+import picamera
+import time
+
+with picamera.PiCamera() as camera:
+   camera.resolution = (640, 480)
+   camera.start_preview()
+   time.sleep(10)
+   camera.capture('/home/pi/image.jpg')
+   camera.stop_preview()
