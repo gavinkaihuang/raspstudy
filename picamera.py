f538d44c9af57e6a61057eb1a20ee50e67dc8641
huangkai <gavinkaihuang@126.com>
2020-03-20 17:38:33 +0800

    1. add TDist

--- picamera.py
+++ picamera.py
@@ -12,7 +12,7 @@
 import picamera
 import time
 
-with picamera.PiCamera() as camera:
+with picamera.camera as camera:
    camera.resolution = (640, 480)
    camera.start_preview()
    time.sleep(10)
