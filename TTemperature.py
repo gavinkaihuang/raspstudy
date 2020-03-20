f23f29960b01ffd226f64b3ee1c11899294c59ea
huangkai <gavinkaihuang@126.com>
2020-03-20 15:14:10 +0800

    1. 测试使用BCM模式

--- TTemperature.py
+++ TTemperature.py
@@ -12,19 +12,24 @@ import Adafruit_DHT
 sensor = Adafruit_DHT.DHT22
 
 def work():
+    GPIO.setmode(GPIO.BCM)
     # BCM number
     pin = 26
-    # 循环
-    while True:
-        try:
-            hu, temp = Adafruit_DHT.read_retry(sensor, pin)
-            # print('temperature:{0:0.1f}°C humidity:{1:0.1f}%'.format(temp,hu))
-            print('温度:{0:0.1f}°C 湿度:{1:0.1f}%'.format(temp, hu))
-            time.sleep(2)
-        except RuntimeError as e:
-            print("error\n{0}".format(e))
-        except:
-            print("error\nFailed to read sensor data!")
+    try:
+        print('开始测量温度')
+        # 循环
+        while True:
+            try:
+                hu, temp = Adafruit_DHT.read_retry(sensor, pin)
+                # print('temperature:{0:0.1f}°C humidity:{1:0.1f}%'.format(temp,hu))
+                print('温度:{0:0.1f}°C 湿度:{1:0.1f}%'.format(temp, hu))
+                time.sleep(2)
+            except RuntimeError as e:
+                print("error\n{0}".format(e))
+            except:
+                print("error\nFailed to read sensor data!")
+    finally:
+        GPIO.cleanup()
 
 if __name__ == '__main__':
     work()
