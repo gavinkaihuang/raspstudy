9a14013d8e76e0e948ff89324b6264057a552608
huangkai <gavinkaihuang@126.com>
2020-03-20 15:04:16 +0800

    1. add TTemperature.py

---
+++ TTemperature.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import RPi.GPIO as GPIO
+import time
+import sys
+import Adafruit_DHT
+
+
+
+# 定义sensor型号为DHT22
+sensor = Adafruit_DHT.DHT22
+
+def work():
+    # BCM number
+    pin = 26
+    # 循环
+    while True:
+        try:
+            hu, temp = Adafruit_DHT.read_retry(sensor, pin)
+            # print('temperature:{0:0.1f}°C humidity:{1:0.1f}%'.format(temp,hu))
+            print('温度:{0:0.1f}°C 湿度:{1:0.1f}%'.format(temp, hu))
+            time.sleep(2)
+        except RuntimeError as e:
+            print("error\n{0}".format(e))
+        except:
+            print("error\nFailed to read sensor data!")
+
+if __name__ == '__main__':
+    work()
