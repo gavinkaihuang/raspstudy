0e3b748303b309a72f537b9896f3a8e91769d9e1
huangkai <gavinkaihuang@126.com>
2020-03-22 23:18:57 +0800

    add saks test code add file saks_timer.py

---
+++ saks/saks_timer.py
@@ -0,0 +1,13 @@
+import time
+import sys
+from sakshat import SAKSHAT
+
+def timer():
+    time_value = time.strftime("%H.%M", time.localtime())
+    saks.digital_display.show(time_value)
+    time.sleep(1)
+
+if __name__ == '__main__':
+
+    saks = SAKSHAT()
+    timer()
