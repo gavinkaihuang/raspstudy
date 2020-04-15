c0c6e1abf1ae795c1628acf42f4b0d3f2829b18c
pi.huang <gavinkaihuang@126.com>
2020-04-15 19:55:58 +0800

    add comments for THuman.py

--- THuman.py
+++ THuman.py
@@ -11,12 +11,12 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-
+GPIO_PIN = 4
 
 def init():
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(4, GPIO.IN)
+    GPIO.setup(GPIO_PIN, GPIO.IN)
 
  
 def detct():
